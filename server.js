79ef0b745d6f986f102b8c1add4b1a42c108e3f1
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-10-22 20:07:55 -0400

    step 3

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/", express.static(__dirname + "/public"));
+
+
 app.get("/heartbeat", (req, res) => {
     res.json({
       is: "working"
