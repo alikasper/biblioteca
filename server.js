61b5eed65fb79d3910be171a17dc72e0f3c5243e
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-10-24 10:23:51 -0400

    update from Thursday

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const routerMain = require('./router/main');
 const app = express();
 
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/", express.static(__dirname + "/public"));
 
+//API routing
+app.use('/', routerMain);
 
 app.get("/heartbeat", (req, res) => {
     res.json({
